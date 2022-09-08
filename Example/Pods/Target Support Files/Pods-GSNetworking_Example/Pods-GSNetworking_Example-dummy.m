531f4bd4c3110824cef120054af8b45d046bbeb0
chenyuanjiang <yuanjiang.chen@capgemini.com>
2022-09-08 14:07:16 +0800

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-GSNetworking_Example/Pods-GSNetworking_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GSNetworking_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GSNetworking_Example
+@end
---
+++ GSNetworking/Classes/GSNetworking.swift
@@ -0,0 +1,16 @@
+//
+//  GSNetworking.swift
+//  GSNetworking_Example
+//
+//  Created by cyj on 2022/9/7.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+class GSNetworking: NSObject {
+    
+    static let shared = GSNetworking()
+    /// 代理
+    ///
+}
